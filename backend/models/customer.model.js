31a911ee1fbe0e5fc3528c5a91c91ba8f3b8bfb7
Nguyen-Hai-Ha <hanguyen032325@gmail.com>
2026-03-20 10:42:36 +0700

    model

---
+++ backend/models/customer.model.js
@@ -0,0 +1,34 @@
+const db = require('../config/database');
+
+const Customer = {
+    getAll: () => {
+        const sql = `SELECT * FROM customers`;
+        const stmt = db.prepare(sql);
+        return stmt.all();
+    },
+    getById: (id) => {
+        const sql = `SELECT * FROM customers WHERE id = ?`;
+        const stmt = db.prepare(sql);
+        return stmt.get(id);
+    },
+    create: (data) => {
+        const sql = `INSERT INTO customers (name, phone, address, cccd, images_cccd) VALUES (?, ?, ?, ?, ?)`;
+        const stmt = db.prepare(sql);
+        const result = stmt.run(data.name, data.phone, data.address, data.cccd, data.images_cccd);
+        return result.lastInsertRowid;
+    },
+    update: (id, data) => {
+        const sql = `UPDATE customers SET name = @name, phone = @phone, address = @address, cccd = @cccd, images_cccd= @images_cccd WHERE id = @id`;
+        const stmt = db.prepare(sql);
+        const result = stmt.run({ ...data, id: id });
+        return result.changes;
+    },
+    delete: (id) => {
+        const sql = `DELETE FROM customers WHERE id = ?`;
+        const stmt = db.prepare(sql);
+        const result = stmt.run(id);
+        return result.changes;
+    }
+}
+
+module.exports = Customer;

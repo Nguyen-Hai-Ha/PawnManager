d8a0c620a6254c8b7684a3ec74f07988abf464c8
Nguyen-Hai-Ha <hanguyen032325@gmail.com>
2026-03-24 17:29:51 +0700

    add change status of schedule

--- backend/models/payment_schedules.model.js
+++ backend/models/payment_schedules.model.js
@@ -39,7 +39,15 @@ const PaymentSchedules = {
         const stmt = db.prepare(sql);
         const result = stmt.run(id);
         return result.changes;
+    },
+    updateStatus: (data, id) => {
+        
+        const sql = `UPDATE payment_schedules SET is_paid = @is_paid WHERE id = @id`;
+        const stmt = db.prepare(sql);
+        const result = stmt.run({ ...data, id: id });
+        return result.changes;
     }
+    
 }
 
 module.exports = PaymentSchedules;

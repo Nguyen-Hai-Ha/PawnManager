5eb5382710c521d1c94d39d6d7a499080c15c766
Nguyen-Hai-Ha <hanguyen032325@gmail.com>
2026-04-02 16:43:22 +0700

    Update role.routes.js

--- backend/routes/role.routes.js
+++ backend/routes/role.routes.js
@@ -3,7 +3,5 @@ const router = express.Router();
 const RoleController = require('../controllers/role.controller');
 
 router.get('/', RoleController.getAll);
-router.post('/', RoleController.create);
-router.delete('/:id', RoleController.delete);
 
 module.exports = router;

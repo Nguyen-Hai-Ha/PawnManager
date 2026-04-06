027e777ca31bd7065212269c2f670840d6ddf835
Nguyen-Hai-Ha <hanguyen032325@gmail.com>
2026-04-06 15:46:48 +0700

    store xem chi tiết HĐ

--- src/stores/contract/detailContract.js
+++ src/stores/contract/detailContract.js
@@ -1,11 +1,13 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
-// import { apiClient } from '@/plugins/axios'
+import { ref, computed } from 'vue';
+import apiClient from '@/plugins/axios'
 
 export const useDetailContractStore = defineStore('detailContract', () => {
+    const detailContract = ref(null)
     const showDetailContract = ref(false);
 
-    const openDetailContract = () => {
+    const openDetailContract = (id) => {
+        getDetailContract(id);
         showDetailContract.value = true;
     };
 
@@ -13,11 +15,72 @@ export const useDetailContractStore = defineStore('detailContract', () => {
         showDetailContract.value = false;
     };
 
+    const formatCurrency = (amount) => {
+        if (amount === undefined || amount === null) return '0';
+
+        const formattedAmount = new Intl.NumberFormat('ni-VN').format(amount);
+
+        return `${formattedAmount} VNĐ`;
+    }
+
+    const collateralmetadata = computed(() => {
+        if (!detailContract.value?.collateral[0]) return [];
+        
+        return detailContract.value?.collateral[0].metadata ? JSON.parse(detailContract.value?.collateral[0].metadata) : [];
+    })
+
+    const collateralImages = computed(() => {
+        if (!detailContract.value?.collateral[0]) return [];
+        
+        let images = detailContract.value?.collateral[0].images;
+        if (typeof images === 'string') {
+            try {
+                return JSON.parse(images);
+            } catch (e) {
+                return [];
+            }
+        }
+        return images || [];
+    })
+
+    const getDetailContract = async (id) => {
+        try {
+            const response = await apiClient.get(`/contract/${id}`);
+            if (response.data.paymentSchedules) {
+                response.data.paymentSchedules = response.data.paymentSchedules.map(item => {
+                    // 1. Lấy dữ liệu history
+                    let historyArr = [];
+                    try {
+                        historyArr = typeof item.payment_history === 'string'
+                            ? JSON.parse(item.payment_history)
+                            : (item.payment_history || []);
+
+                        historyArr = Array.isArray(historyArr) ? historyArr.filter(p => p && p.amount > 0) : [];
+                    } catch (e) { historyArr = []; }
+                    item.display_history = historyArr
+                        .map(p => `${formatCurrency(p.amount)} - (${p.created_at})`)
+                        .join(', ');
+
+                    return item;
+                });
+            }
+            detailContract.value = response.data;
+        } catch (error) {
+            console.error('Error fetching detail contract:', error);
+        }
+    };
+
     return {
         //state
-        showDetailContract,
+        showDetailContract, detailContract,
+
+        //computed
+        collateralmetadata, collateralImages,
 
         //actions
-        openDetailContract, closeDetailContract
+        openDetailContract, closeDetailContract, formatCurrency, 
+
+        //fetch
+        getDetailContract
     };
 });

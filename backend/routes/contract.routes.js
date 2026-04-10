4bae393a4c07db7aa69b874314b9f27b6953534c
Putu Wibowo <hanguyen032325@gmail.com>
2026-04-10 17:00:47 +0700

    BE hàm xuất excel trong controller, modal, api route

--- backend/routes/contract.routes.js
+++ backend/routes/contract.routes.js
@@ -6,11 +6,11 @@ const upload = require('../middlewares/upload.middleware');
 
 router.get('/', verifyToken, hasPermission(['loans.read', 'pledge.read', 'repayment.read']), ContractController.getAll);
 router.get('/type/:id', verifyToken, hasPermission(['loans.read', 'pledge.read', 'repayment.read']), ContractController.getByIdContractType);
+router.get('/export', verifyToken, hasPermission(['loans.export', 'pledge.export', 'repayment.export']), ContractController.exportExcel);
 router.get('/:id', verifyToken, hasPermission(['loans.detail', 'pledge.detail', 'repayment.detail']), ContractController.getById);
 router.get('/:id/payment-details', verifyToken, hasPermission(['loans.detail', 'pledge.detail', 'repayment.detail']), ContractController.getPaymentDetails);
 router.post('/', verifyToken, hasPermission(['loans.create', 'pledge.create', 'repayment.create']), upload.array('images', 10), ContractController.create);
 router.delete('/:id', verifyToken, hasPermission('contract.delete'), ContractController.delete);
 router.get('/:id/print', verifyToken, hasPermission(['loans.print', 'pledge.print', 'repayment.print']), ContractController.printReceipt);
-// router.get('/:id/print-receipt', verifyToken, hasPermission(['loans.print', 'pledge.print', 'repayment.print']), ContractController.printReceipt);
 router.get('/:id/settlement-detail', verifyToken, hasPermission(['loans.final_settlement', 'pledge.final_settlement', 'repayment.final_settlement']), ContractController.getSettlementDetail);
 module.exports = router

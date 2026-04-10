817f5e13b86a116263394680030eb72fd6fc2629
Putu Wibowo <hanguyen032325@gmail.com>
2026-04-10 14:24:49 +0700

    sửa pemission

--- backend/routes/settings.routes.js
+++ backend/routes/settings.routes.js
@@ -6,12 +6,12 @@ const { verifyToken, hasPermission } = require('../middlewares/auth.middleware')
 const uploadFile = require('../middlewares/uploadFile.middleware');   
 
 router.get('/', verifyToken, hasPermission('settings.view'), getSettings);
-router.get('/templates', verifyToken, hasPermission('settings.view'), SettingController.getAllTemplates);
-router.get('/templates/:id', verifyToken, hasPermission('settings.view'), SettingController.getTemplateById);
-router.post('/templates', verifyToken, hasPermission('settings.create'), uploadFile.single('file_path'), SettingController.createTemplates);
-router.put('/templates/:id', verifyToken, hasPermission('settings.update'), uploadFile.single('file_path'), SettingController.updateTemplate);
-router.delete('/templates/:id', verifyToken, hasPermission('settings.delete'), SettingController.deleteTemplate);
+router.get('/templates', verifyToken, hasPermission('settings.template_view'), SettingController.getAllTemplates);
+router.get('/templates/:id', verifyToken, hasPermission('settings.template_detail'), SettingController.getTemplateById);
+router.post('/templates', verifyToken, hasPermission('settings.template_create'), uploadFile.single('file_path'), SettingController.createTemplates);
+router.put('/templates/:id', verifyToken, hasPermission('settings.template_update'), uploadFile.single('file_path'), SettingController.updateTemplate);
+router.delete('/templates/:id', verifyToken, hasPermission('settings.template_delete'), SettingController.deleteTemplate);
 router.put('/', verifyToken, hasPermission('settings.update'), updateSettings);
-router.get('/templates/:id/download', verifyToken, hasPermission('settings.print'), SettingController.downLoadTemplate);
+router.get('/templates/:id/download', verifyToken, hasPermission('settings.template_download'), SettingController.downLoadTemplate);
 
 module.exports = router;

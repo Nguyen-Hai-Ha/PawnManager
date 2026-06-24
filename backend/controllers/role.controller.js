b497edd970909507a863bee847fc700df1fe23f3
Nguyen-Hai-Ha <hanguyen032325@gmail.com>
2026-06-24 16:09:22 +0700

    bỏ try catch trong RoleController

--- backend/controllers/role.controller.js
+++ backend/controllers/role.controller.js
@@ -2,34 +2,22 @@ const { Role, RolePermission } = require('../models');
 
 const RoleController = {
     getAll: (req, res) => {
-        try {
-            const roles = Role.getAll();
-            res.json(roles);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
+        const roles = Role.getAll();
+        res.json(roles);
     },
     getPermissionByRoleId: (req, res) => {
-        try {
-            const { id } = req.params;
-            const permissions = RolePermission.getPermissionByRoleId(id);
-            if (!permissions) {
-                return res.status(403).json({ error: 'Permissions of role not found' });
-            }
-            res.json(permissions);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
+        const { id } = req.params;
+        const permissions = RolePermission.getPermissionByRoleId(id);
+        if (!permissions) {
+            return res.status(403).json({ error: 'Permissions of role not found' });
         }
+        res.json(permissions);
     },
     updateRolePermissions: (req, res) => {
-        try {
-            const { id } = req.params;
-            const { permissionIds } = req.body;
-            const result = RolePermission.updateRolePermissions(id, permissionIds);
-            res.json(result);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
+        const { id } = req.params;
+        const { permissionIds } = req.body;
+        const result = RolePermission.updateRolePermissions(id, permissionIds);
+        res.json(result);
     }
 }
 

5b46a24623bea45c7d8ad4303aca552e6d140cd7
Nguyen-Hai-Ha <hanguyen032325@gmail.com>
2026-06-24 14:59:03 +0700

    bỏ try catch trong CollateralTypeController

--- backend/controllers/collateral_type.controller.js
+++ backend/controllers/collateral_type.controller.js
@@ -2,44 +2,24 @@ const { CollateralsType } = require('../models');
 
 const CollateralTypeController = {
     getAll: (req, res) => {
-        try {
             const collateralTypes = CollateralsType.getAll();
             res.json(collateralTypes);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
     },
     getById: (req, res) => {
-        try {
             const collateralType = CollateralsType.getById(req.params.id);
             res.json(collateralType);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
     },
     create: (req, res) => {
-        try {
             const collateralType = CollateralsType.create(req.body);
             res.json(collateralType);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
     },
     update: (req, res) => {
-        try {
             const collateralType = CollateralsType.update(req.params.id, req.body);
             res.json(collateralType);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
     },
     delete: (req, res) => {
-        try {
             const collateralType = CollateralsType.delete(req.params.id);
             res.json(collateralType);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
     }
 }
 

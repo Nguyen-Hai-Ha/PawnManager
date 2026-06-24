5a059132e556e135eaae4751229c277eb4bf977b
Nguyen-Hai-Ha <hanguyen032325@gmail.com>
2026-06-24 15:00:10 +0700

    bỏ try catch trong ContractTypesController

--- backend/controllers/contracts_types.controller.js
+++ backend/controllers/contracts_types.controller.js
@@ -2,28 +2,16 @@ const { ContractType } = require('../models');
 
 const ContractTypesController = {
     getAll: (req, res) => {
-        try {
             const contractsTypes = ContractType.getAll();
             res.json(contractsTypes);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
     },
     getById: (req, res) => {
-        try {
             const contractsType = ContractType.getById(req.params.id);
             res.json(contractsType);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
     },
     create: (req, res) => {
-        try {
             const contractsType = ContractType.create(req.body);
             res.json(contractsType);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
     }
 }
 

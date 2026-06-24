55b27885d21ec7570157aefc1513a625c4742f1e
Nguyen-Hai-Ha <hanguyen032325@gmail.com>
2026-06-24 16:11:38 +0700

    bỏ try catch trong TransactionTypeController

--- backend/controllers/transactions_type.controller.js
+++ backend/controllers/transactions_type.controller.js
@@ -2,29 +2,17 @@ const { TransactionType }  = require('../models');
 
 const TransactionTypeController = {
     getAll: (req, res) => {
-        try {
-            const transactionTypes = TransactionType.getAll();
-            res.json(transactionTypes);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
+        const transactionTypes = TransactionType.getAll();
+        res.json(transactionTypes);
     },
     create: (req, res) => {
-        try {
-            const data = req.body;
-            const transactionType = TransactionType.create(data);
-            res.json(transactionType);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
+        const data = req.body;
+        const transactionType = TransactionType.create(data);
+        res.json(transactionType);
     },
     delete: (req, res) => {
-        try {
-            const transactionType = TransactionType.delete(req.params.id);
-            res.json(transactionType);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
+        const transactionType = TransactionType.delete(req.params.id);
+        res.json(transactionType);
     }
 }
 

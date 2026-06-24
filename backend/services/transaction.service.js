14e4bab4fcdaf2db18eea11b91dc186427e74d6d
Nguyen-Hai-Ha <hanguyen032325@gmail.com>
2026-06-24 17:31:15 +0700

    thêm hàm finalsettlementContract cho service

--- backend/services/transaction.service.js
+++ backend/services/transaction.service.js
@@ -304,6 +304,102 @@ const TransactionsService = {
            return { transaction, current_schedule, contractHistory, msg: "Cập nhật gốc và lãi thành công" };
         });
         return exp(data);
-    }
+    },
+    finalsettlementContract:(data) => {
+        const exp = db.transaction(async(data)=>{
+            const amount = data.amount;
+            const other_fees = data.other_fees || 0;
+            const id_contract = data.id_contract;
+            const id_staff = data.id_staff;
+            const description = data.description;
 
+
+            const contract = Contract.getById(id_contract);
+            if (!contract) return { error: "Hợp đồng không tồn tại" };
+
+            // Lấy tất cả kỳ chưa đóng của hợp đồng này (được sắp xếp theo period_number tăng dần)
+            const schedules = PaymentSchedules.getByContractId(id_contract).filter(s => s.is_paid === 0).sort((a, b) => a.period_number - b.period_number);
+
+            if (schedules.length === 0) {
+                return { error: "Hợp đồng đã hoàn tất" };
+            }
+            // Tạo giao dịch loại 3 (tất toán)
+            const transaction = Transactions.create({
+                amount: amount,
+                other_fees: other_fees,
+                description: description,
+                id_contract: id_contract,
+                id_transaction_type: 3,
+                id_staff: id_staff
+            });
+
+            // Cập nhật trạng thái của hợp đồng
+            Contract.updateStatus({ status: 'Đã Tất Toán' }, id_contract);
+
+            const collateral = Collaterals.getByContractId(contract.id);
+            if (collateral) {
+                collateral.forEach(item => {
+                    Collaterals.updateStatus({ status: 'Đã Chuộc' }, item.id);
+                })
+            }
+
+            // Cập nhật trạng thái của tất cả các kỳ
+            schedules.forEach(schedule => {
+                PaymentSchedules.updateStatus({ is_paid: 1 }, schedule.id);
+            });
+
+            const staff = Staff.getById(id_staff);
+
+            AuditLogs.create({
+                action: 'Tất toán Hợp đồng',
+                details: `Tất toán Hợp đồng ${contract.code} với số tiền ${amount} bởi nhân viên ${staff.name}`,
+                id_staff: staff.id,
+            });
+
+            return { transaction, msg: "Hợp đồng đã được tất toán" };
+        });
+        return exp(data);
+    },
+    liquidationContract:(data) => {
+        const exp = db.transaction(async(data) => {
+            const amount = data.amount;
+            const id_contract = data.id_contract;
+            const id_collateral = data.id_collateral;
+            const id_staff = data.id_staff
+
+            const contract = Contract.getById(id_contract);
+            if (!contract) return { error: "Hợp đồng không tồn tại" };
+
+            // Tạo giao dịch loại 5 (thanh lý)
+            const transaction = Transactions.create({
+                amount: amount,
+                other_fees: 0,
+                id_contract: id_contract,
+                id_transaction_type: 5,
+                id_staff: id_staff
+            });
+
+            // Cập nhật trạng thái của hợp đồng
+            Contract.updateStatus({ status: 'Đã Thanh Lý' }, id_contract);
+
+            // Cập nhật trạng thái của tài sản
+            const collateral = Collaterals.getById(id_collateral);
+            if (collateral) {
+                Collaterals.updateStatus({ status: 'Đã Thanh Lý' }, collateral.id);
+            } else {
+                return { error: "Tài sản không tồn tại" };
+            }
+
+            const staff = Staff.getById(id_staff);
+
+            AuditLogs.create({
+                action: 'Thanh lý Tài Sản',
+                details: `Thanh lý Tài Sản của Hợp đồng ${contract.code} với số tiền ${amount} bởi ${staff.name}`,
+                id_staff: staff.id,
+            });
+
+            return { transaction, msg: "Tài sản đã được thanh lý" };
+        });
+        return exp(data);
+    },
 }

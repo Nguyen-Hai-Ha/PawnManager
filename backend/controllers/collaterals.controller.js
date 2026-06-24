b6c468b82fe5c4c05ad18e4afe5bf39309bad8c1
Nguyen-Hai-Ha <hanguyen032325@gmail.com>
2026-06-24 14:59:51 +0700

    bỏ try catch trong CollateralsController

--- backend/controllers/collaterals.controller.js
+++ backend/controllers/collaterals.controller.js
@@ -2,40 +2,23 @@ const { Collaterals, AuditLogs, Contract } = require('../models');
 
 const CollateralsController = {
     getAll: (req, res) => {
-        try {
             const collaterals = Collaterals.getAll();
             res.json(collaterals);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
     },
     getById: (req, res) => {
-        try {
             const collateral = Collaterals.getById(req.params.id);
             res.json(collateral);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
     },
     getLiquidationById: (req, res) => {
-        try {
             const collateral = Collaterals.getLiquidationById(req.params.id);
             res.json(collateral);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
     },
     create: (req, res) => {
-        try {
             const data = req.body;
             const collateral = Collaterals.create(data);
             res.json(collateral);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
     },
     update: (req, res) => {
-        try {
             const data = req.body;
             const { id } = req.params;
 
@@ -57,25 +40,14 @@ const CollateralsController = {
             }
 
             res.json({ message: "Cập nhật tài sản thành công" });
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
     },
     delete: (req, res) => {
-        try {
             const collateral = Collaterals.delete(req.params.id);
             res.json(collateral);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
     },
     countLiquidation: (req, res) => {
-        try {
             const count = Collaterals.countLiquidation();
             res.json(count);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
     }
 }
 
